3b690196a5b4b2269fd06b324b45472ee7f7244a
DESKTOP-EHH0K4D\SnowRang <ajantang@naver.com>
2025-01-07 16:59:20 +0900

    :sparkles: [Feat] 404 리턴시 EmptyList 컴포넌트 출력

--- src/app/(mover)/mover/my-quote/[quoteId]/page.tsx
+++ src/app/(mover)/mover/my-quote/[quoteId]/page.tsx
@@ -2,6 +2,8 @@ import { cookies } from "next/headers";
 
 import { getSentQuoteDetail } from "@/api/quote";
 import SentQuoteDetail from "./SentQuoteDetail";
+import EmptyList from "@/components/EmptyList";
+import { AxiosError } from "axios";
 
 export interface MyQuotesDetailPageProps {
   params: Promise<{
@@ -17,8 +19,6 @@ export default async function MyQuoteDetailPage({
   const cookieStore = await cookies();
   const cookie = `accessToken=${cookieStore.get("accessToken")?.value}`;
 
-  const data = await getSentQuoteDetail({ cookie, quoteId: Number(quoteId) });
-
   const styles = {
     constainer: `flex flex-col items-center w-full`,
     topBar: `flex flex-row gap-2.5 items-center justify-center min-w-[328px] w-full h-[54px] 
@@ -29,12 +29,36 @@ export default async function MyQuoteDetailPage({
       pc:text-2xl`,
   };
 
-  return (
-    <div className={styles.constainer}>
-      <div className={styles.topBar}>
-        <div className={styles.barItem}>견적 상세</div>
+  try {
+    const data = await getSentQuoteDetail({ cookie, quoteId: Number(quoteId) });
+
+    return (
+      <div className={styles.constainer}>
+        <div className={styles.topBar}>
+          <div className={styles.barItem}>견적 상세</div>
+        </div>
+        <SentQuoteDetail data={data} />
       </div>
-      <SentQuoteDetail data={data} />
-    </div>
-  );
+    );
+  } catch (err: any) {
+    console.error(err);
+    let text = "견적 상세 조회에 실패했습니다.";
+
+    if (err.status === 404) {
+      text = err.message || "견적을 찾을 수 없습니다.";
+    }
+
+    if (err instanceof AxiosError) {
+      text = err.response?.data.message || "견적 상세 조회에 실패했습니다.";
+    }
+
+    return (
+      <div className={styles.constainer}>
+        <div className={styles.topBar}>
+          <div className={styles.barItem}>견적 상세</div>
+        </div>
+        <EmptyList text={text} />
+      </div>
+    );
+  }
 }
--- src/app/(user)/my-quote/[quoteId]/page.tsx
+++ src/app/(user)/my-quote/[quoteId]/page.tsx
@@ -3,6 +3,8 @@ import { cookies } from "next/headers";
 import QuoteDetail from "./QuoteDetail";
 import { getQuote } from "@/api/quote";
 import { GetQuoteApiResponseData } from "@/types/api";
+import EmptyList from "@/components/EmptyList";
+import { AxiosError } from "axios";
 
 export interface MyQuotesDetailPageProps {
   params: Promise<{
@@ -18,8 +20,6 @@ export default async function MyQuotesDetailPage({
   const cookieStore = await cookies();
   const cookie = `accessToken=${cookieStore.get("accessToken")?.value}`;
 
-  const data = await getQuote({ cookie, quoteId: Number(quoteId) });
-
   const styles = {
     constainer: `flex flex-col items-center w-full`,
     topBar: `flex flex-row gap-2.5 items-center justify-center min-w-[328px] w-full h-[54px] 
@@ -30,12 +30,36 @@ export default async function MyQuotesDetailPage({
       pc:text-2xl`,
   };
 
-  return (
-    <div className={styles.constainer}>
-      <div className={styles.topBar}>
-        <div className={styles.barItem}>견적 상세</div>
+  try {
+    const data = await getQuote({ cookie, quoteId: Number(quoteId) });
+
+    return (
+      <div className={styles.constainer}>
+        <div className={styles.topBar}>
+          <div className={styles.barItem}>견적 상세</div>
+        </div>
+        <QuoteDetail data={data} />
       </div>
-      <QuoteDetail data={data} />
-    </div>
-  );
+    );
+  } catch (err: any) {
+    console.error(err);
+    let text = "견적 상세 조회에 실패했습니다.";
+
+    if (err.status === 404) {
+      text = err.message || "견적을 찾을 수 없습니다.";
+    }
+
+    if (err instanceof AxiosError) {
+      text = err.response?.data.message || "견적 상세 조회에 실패했습니다.";
+    }
+
+    return (
+      <div className={styles.constainer}>
+        <div className={styles.topBar}>
+          <div className={styles.barItem}>견적 상세</div>
+        </div>
+        <EmptyList text="견적 상세 조회에 실패했습니다." />
+      </div>
+    );
+  }
 }
