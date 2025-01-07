51ee9b85041c359e0f2d00dcd8874321680a768f
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2025-01-07 11:46:31 +0900

    :bug: [fix] RoleGuard info-edit url 코드 제거

--- src/components/auth/RoleGuard.tsx
+++ src/components/auth/RoleGuard.tsx
@@ -53,14 +53,6 @@ export default function RoleGuard({
         return;
       }
 
-      if (
-        pathname.startsWith("/me/info-edit") &&
-        pathname.includes("/mover/info-edit") &&
-        useUserStore.getState().isOAuth
-      ) {
-        return;
-      }
-
       try {
         const userInfo = await getUserInfo();
         const userRole = userInfo.user.mover
